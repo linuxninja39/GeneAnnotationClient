1427a94d278f03badda826e2653be2b944caba6f
Jacob Boswell <jacob@s56.net>
2017-05-25 18:51:42 -0600

    a little clean up

--- src/app/components/genes/genes.component.ts
+++ src/app/components/genes/genes.component.ts
@@ -5,7 +5,7 @@ import { Log } from 'ng2-logger';
 
 const log = Log.create('GeneService');
 
-declare var jlinq: any;
+declare const jlinq: any;
 
 @Component({
   selector: 'app-genes',
@@ -16,7 +16,7 @@ export class GenesComponent implements OnInit {
   genesOrig: GeneModel[];
   genes: GeneModel[];
 
-  constructor(private geneService: GeneService, private changeDetector: ChangeDetectorRef) { }
+  constructor(private geneService: GeneService) { }
 
   ngOnInit() {
     this.geneService
@@ -30,18 +30,6 @@ export class GenesComponent implements OnInit {
     ;
   }
 
-  search(event: any) {
-    const searchTerm = event.target.value;
-    this.genes = jlinq.from(this.genesOrig)
-      .match('firstName', searchTerm)
-      .orMatch('lastName', searchTerm)
-      .orMatch('email', searchTerm)
-      .select();
-
-    this.changeDetector.detectChanges();
-  }
-
-
   searchGenes(event) {
     const searchTerm = event.target.value;
     log.error('event', event);
@@ -51,7 +39,5 @@ export class GenesComponent implements OnInit {
       .orMatch('hgncId', searchTerm)
       .orMatch('geneNameExpansion', searchTerm)
       .select();
-
-    this.changeDetector.detectChanges();
   }
 }
