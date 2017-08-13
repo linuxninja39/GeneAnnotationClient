23c74e33a96ea3e5628eb9c455bb26df80f2dadc
Jacob Boswell <jacob@s56.net>
2017-08-13 12:14:22 -0600

    addGeneVariantAnnotation test done and passing

--- src/app/services/annotation.service.spec.ts
+++ src/app/services/annotation.service.spec.ts
@@ -75,4 +75,51 @@ describe('AnnotationService', () => {
       }
     )
   );
+
+    it(
+    'addGeneVariantAnnotation should post to correct url',
+    inject(
+      [
+        XHRBackend,
+        AnnotationService
+      ],
+      (mockBackend: MockBackend, service: AnnotationService) => {
+        const controlAnnotation: AnnotationModel = JSON.parse(JSON.stringify(TestAnnotations[0]));
+        const passedAnnotation: AnnotationModel = JSON.parse(JSON.stringify(TestAnnotations[0]));
+        let lastConnection: MockConnection;
+        mockBackend.connections.subscribe(
+          (connection: MockConnection) => {
+            lastConnection = connection;
+            const resOptions = new ResponseOptions({body: JSON.stringify(controlAnnotation)});
+            const res = new Response(resOptions);
+            connection.mockRespond(res);
+          }
+        );
+        const geneVariantId = 1;
+        service
+          .addGeneVariantAnnotation(
+            geneVariantId,
+            passedAnnotation
+          )
+          .subscribe(
+            (annotation: AnnotationModel) => {
+              expect(lastConnection.request.url).toBe(
+                sprintf(AnnotationService.ADD_GENE_VARIANT_ANNOTATION_EP, geneVariantId)
+              );
+              expect(lastConnection.request.method).toBe(RequestMethod.Post);
+              controlAnnotation.appUserId = controlAnnotation.appUser.id;
+              controlAnnotation.appUser = null;
+              expect(
+                JSON.stringify(
+                  JSON.parse(
+                    lastConnection.request.getBody()
+                  )
+                )
+              ).toBe(JSON.stringify(controlAnnotation));
+            }
+          );
+
+      }
+    )
+  );
 });
--- src/app/services/annotation.service.ts
+++ src/app/services/annotation.service.ts
@@ -6,22 +6,32 @@ import {Http} from '@angular/http';
 import {Observable} from 'rxjs/Observable';
 import {sprintf} from 'sprintf-js';
 import {Log} from 'ng2-logger';
+import {GeneVariantModel} from '../models/api/gene-variant.model';
 
 const log = Log.create('AnnotationService');
 
 @Injectable()
 export class AnnotationService {
-  public static BASE_EP: string = environment.apiServerUrl + '/Annotations';
+  public static BASE_EP = environment.apiServerUrl + '/Annotations';
   public static ADD_GENE_ANNOTATION_EP = AnnotationService.BASE_EP + '/gene/%s';
+  public static ADD_GENE_VARIANT_ANNOTATION_EP = AnnotationService.BASE_EP + '/GeneVariant/%s';
 
   constructor(private http: Http) {}
 
   addGeneAnnotations(geneId: string | number, annotation: AnnotationModel): Observable<AnnotationModel> {
     const url = sprintf(AnnotationService.ADD_GENE_ANNOTATION_EP, geneId);
-    annotation.appUserId = annotation.appUser.id;
-    annotation.appUser = null;
-    log.info('submitting annotation on gene', annotation, geneId);
-    return this.http.post(url, annotation).map((r) => r.json());
+    return this.postAnnotation(url, geneId, annotation);
   }
 
+  addGeneVariantAnnotation(geneVariantId: string | number, annotation: AnnotationModel): Observable<AnnotationModel> {
+    const url = sprintf(AnnotationService.ADD_GENE_VARIANT_ANNOTATION_EP, geneVariantId, annotation);
+    return this.postAnnotation(url, geneVariantId, annotation);
+  }
+
+  private postAnnotation(url: string, id: string | number, annotation: AnnotationModel): Observable<AnnotationModel> {
+    annotation.appUserId = annotation.appUser.id;
+    annotation.appUser = null;
+    log.info('posting annotation', annotation, id);
+    return this.http.post(url, annotation).map((r) => r.json());
+  }
 }
