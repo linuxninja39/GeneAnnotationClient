42877f9935e842612bcc21d40b678625a2533b67
Jacob Boswell <jacob@s56.net>
2017-09-13 15:34:14 -0600

    added test for geneVariantLiterature table and passing

--- src/app/components/gene-variant-literature-data-table/gene-variant-literature-data-table.component.spec.ts
+++ src/app/components/gene-variant-literature-data-table/gene-variant-literature-data-table.component.spec.ts
@@ -1,6 +1,6 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { GeneVariantLiteratureDataTableComponent } from './gene-variant-literature-data-table.component';
+import {GeneVariantLiteratureDataTableComponent} from './gene-variant-literature-data-table.component';
 import {DataTableModule, DialogModule, DropdownModule, ListboxModule} from 'primeng/primeng';
 import {TruncateWordsPipe} from 'ng2-truncate/dist/truncate-words.pipe';
 import {MdCardModule} from '@angular/material';
@@ -17,16 +17,44 @@ import {
 } from '../gene-variant-literature-form-dialog/gene-variant-literature-form-dialog.component';
 import {GeneVariantLiteratureService} from '../../services/gene-variant-literature.service';
 import {TestGeneVariants} from '../../test-data/test-gene-variants.spec';
+import {By} from '@angular/platform-browser';
+import {Component, Input} from '@angular/core';
+import {DatePipe} from '@angular/common';
+
+
+@Component({
+  selector: 'app-gene-variant-literature-form-dialog',
+  template: ''
+})
+class TestFormDialogComponent {
+  @Input()
+  display;
+  @Input()
+  geneVariant;
+}
 
 describe('GeneVariantLiteratureDataTableComponent', () => {
   let component: GeneVariantLiteratureDataTableComponent;
   let fixture: ComponentFixture<GeneVariantLiteratureDataTableComponent>;
+  let literatureService: LiteratureService;
+
+  const testGeneVariantIndex = 0;
+  const correctColumns = [
+    'Title',
+    'Url',
+    'PubMed ID',
+    'Abstract',
+    'Added On',
+    'Added By',
+    'Pathogenic Support Category',
+    'Annotations',
+  ];
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         GeneVariantLiteratureDataTableComponent,
-        GeneVariantLiteratureFormDialogComponent,
+        TestFormDialogComponent,
         TruncateWordsPipe
       ],
       imports: [
@@ -42,24 +70,78 @@ describe('GeneVariantLiteratureDataTableComponent', () => {
         DropdownModule
       ],
       providers: [
-        LiteratureService,
         AnnotationService,
         AuthService,
-        CookieService,
-        GeneVariantLiteratureService
+        CookieService
       ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(GeneVariantLiteratureDataTableComponent);
     component = fixture.componentInstance;
-    component.geneVariant = JSON.parse(JSON.stringify(TestGeneVariants[0]));
+    component.geneVariant = JSON.parse(JSON.stringify(TestGeneVariants[testGeneVariantIndex]));
+    // literatureService = fixture.debugElement.injector.get(LiteratureService);
+
     fixture.detectChanges();
   });
 
   it('should be created', () => {
-     expect(component).toBeTruthy();
+    expect(component).toBeTruthy();
   });
+
+  it(
+    'should have correct columns in table',
+    () => {
+      const dataTableElement = fixture.debugElement.query(By.css('thead'));
+      const columns = dataTableElement.children[0].children;
+
+      expect(columns.length).toEqual(correctColumns.length, 'column count should match');
+      for (let i = 0; i < correctColumns.length; i++) {
+        expect(columns[i].nativeElement.innerText).toEqual(correctColumns[i]);
+      }
+    }
+  );
+
+  it(
+    'html rows and cells should match geneVariant.geneVariantLiterature',
+    () => {
+      const dataTableElement = fixture.debugElement.query(By.css('tbody'));
+      const rows = dataTableElement.children;
+      const geneVariantLiteratures = TestGeneVariants[testGeneVariantIndex].geneVariantLiterature;
+      expect(rows.length)
+        .toBe(geneVariantLiteratures.length, 'table size and number of variantLiteratures mismatch');
+
+      /*
+      [
+    'Title',
+    'Url',
+    'PubMed ID',
+    'Abstract',
+    'Added On',
+    'Added By',
+    'Pathogenic Support Category',
+    'Annotations',
+  ];
+       */
+
+      const datePipe = new DatePipe('en-US');
+
+      const rowCount = 0;
+      for (let i = 0; i < rows.length; i++) {
+        const row = rows[i];
+        const cells = row.children;
+        const addedOnString = datePipe.transform(geneVariantLiteratures[i].addedAt, 'medium');
+
+        expect(cells[0].nativeElement.innerText).toContain(geneVariantLiteratures[i].literature.title);
+        expect(cells[1].nativeElement.innerText).toContain(geneVariantLiteratures[i].literature.url);
+        expect(cells[2].nativeElement.innerText).toContain(geneVariantLiteratures[i].literature.pubMedId);
+        expect(cells[3].nativeElement.innerText).toContain(geneVariantLiteratures[i].literature.details);
+        expect(cells[4].nativeElement.innerText).toContain(addedOnString);
+        expect(cells[5].nativeElement.innerText).toContain(geneVariantLiteratures[i].appUser.name);
+        expect(cells[6].nativeElement.innerText).toContain(geneVariantLiteratures[i].pathogenicSupportCategory.name);
+      }
+    }
+  );
 });
