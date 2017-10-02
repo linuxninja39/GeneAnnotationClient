a0043b7a8318aefaefb4e99dd89219bbe360bf49
Jacob Boswell <jacob@s56.net>
2017-10-02 15:24:22 -0600

    author model adjustments

--- src/app/models/api/author.model.ts
+++ src/app/models/api/author.model.ts
@@ -1,8 +1,12 @@
 import {BaseModel} from './base.model';
+import {AnnotationModel} from './annotation.model';
+import {LiteratureModel} from './literature.model';
 /**
  * Created by jboswell on 5/30/2017.
  */
 
 export interface AuthorModel extends BaseModel {
   name: string;
+  annotations?: AnnotationModel[];
+  literatures?: LiteratureModel[];
 }
